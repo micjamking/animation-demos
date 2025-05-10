e245b2e34a024f07a0d43f9977206c5ad72d7f80
Mike King <hello@mikeking.io>
2025-05-09 20:25:12 -1000

    Update rotation GLSL demo

--- demos/shaders/glsl/sandbox/README.md
+++ demos/shaders/glsl/sandbox/README.md
@@ -17,3 +17,6 @@
 ### 3D
 
 - [ ] Create cube with lighting
+
+### Aspect ratio and responsive scaling/positioning
+Keeping the size and positioning of elements resolution independent is a matter of scaling the coordinates by the aspect ratio of the screen. This is done by multiplying the x coordinate by the aspect ratio (width / height) before passing it to the shader. The size of the polygon is also scaled by the aspect ratio to maintain its size relative to the screen dimensions. The position of the polygon is adjusted to be in the center of the screen, taking into account the aspect ratio. This ensures that the polygon appears in the same relative position and size on screens with different resolutions.
--- demos/shaders/glsl/sandbox/animation/rotation.glsl
+++ demos/shaders/glsl/sandbox/animation/rotation.glsl
@@ -9,7 +9,7 @@ uniform float u_time;
 float borderSize = 0.001;
 float numberOfSides = 5.0;
 float delay = 0.05;
-float gap = 0.025;
+float gap = 0.0125;
 
 float round(float n){
   return floor(0.5 + n);
@@ -31,7 +31,7 @@ float myPolygonShape(vec2 uv, float nSides, float size, vec2 pos, bool rotate, f
 }
 
 float myPolygon(vec2 uv, float nSides, float size, float aspect, vec2 pos, bool rotate, float rotationDelay){
-  return (myPolygonShape(uv, nSides, size * aspect, pos, rotate, rotationDelay) * (1.0 - myPolygonShape(uv, nSides, (size - borderSize) * aspect, pos, rotate, rotationDelay)));
+  return (myPolygonShape(uv, nSides, size, pos, rotate, rotationDelay) * (1.0 - myPolygonShape(uv, nSides, (size - borderSize), pos, rotate, rotationDelay)));
 }
 
 void main() {
@@ -42,12 +42,10 @@ void main() {
   vec3 col = 0.5 + 0.5 * sin(u_time + uv.xyx + vec3(0.0, 2.0, 4.0));
   float polygon = 0.0;
   for (int i = 0; i < 100; i++) {
-    float size = float(i) * gap;
+    float size = float(i) * (gap);
     float rotationDelay = float(i) * delay;
     polygon += myPolygon(uv, numberOfSides, size, aspect, vec2(center, 0.5), true, rotationDelay);
   }
   col = col * vec3(polygon);
   gl_FragColor = vec4( col, 1.0 );
 }
-
-// Keeping the size and positioning of elements resolution independent is a matter of scaling the coordinates by the aspect ratio of the screen. This is done by multiplying the x coordinate by the aspect ratio (width / height) before passing it to the shader. The size of the polygon is also scaled by the aspect ratio to maintain its size relative to the screen dimensions. The position of the polygon is adjusted to be in the center of the screen, taking into account the aspect ratio. This ensures that the polygon appears in the same relative position and size on screens with different resolutions.
